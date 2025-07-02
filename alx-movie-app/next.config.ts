951c3fff742a89bde27ddadc3eb26766b9d915b5
ahmed <ahmedabdelnaser3010@gmail.com>
2025-07-03 00:39:02 +0300

    task two

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,15 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-        port: '',
-        pathname: '/images/**',
-      },
-    ],
-  },
-}
+import withPWAInit from "@ducanh2912/next-pwa";
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'],
+  },
+};
+
+export default withPWA({
+  ...nextConfig
+})
